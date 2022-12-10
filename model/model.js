bebfb03b7eeeeb2c4c53a33e312873357f5a3cb1
yash-bele <yashgbele@gmail.com>
2022-12-10 11:25:36 +0530

    commit

--- model/model.js
+++ model/model.js
@@ -15,27 +15,3 @@ const schema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('collection', schema);
-
-// {
-//     "id": "1"
-//     "cards": [
-//         {
-//             "id": "1",
-//             "title": "Hello",
-//             "note": "Hello, my name is Yash.",
-//             "pinned": false,
-//             "cardTags": [
-//                 {
-//                     "id": "111",
-//                     "title": "new"
-//                 }
-//             ]
-//         }
-//     ],
-//     "tags": [
-//         {
-//             "id": "11",
-//             "title": "new"
-//         }
-//     ]
-// }
